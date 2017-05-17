77e46aaaeb621f97da0321949c56f5effae5b155
Mateusz Ośko <matexo15@gmail.com>
2017-05-18 00:52:20 +0200

    Wrzutka do maina w razie czego

--- src/main/java/tripApp/Main.java
+++ src/main/java/tripApp/Main.java
@@ -1,13 +1,20 @@
 package tripApp;
 
+import com.microsoft.azure.storage.StorageException;
+import tripApp.config.StaticConfig;
 import tripApp.queue.ResizeQueue;
+
+import java.net.URISyntaxException;
+import java.security.InvalidKeyException;
 
 /**
  * Created by piotr on 17.05.17.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidKeyException, StorageException, URISyntaxException {
 
         //Thread resizeThread = new Thread(new ResizeQueue())
+        ResizeQueue resizeQueue = new ResizeQueue(StaticConfig.getThumbnailGenQueueAzureConfig() , StaticConfig.getThumbnailBlobAzureConfig());
+        resizeQueue.run();
     }
 }
