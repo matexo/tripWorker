08a9413d62bd4da0d3ecbee434df3f5b16bd0375
piotter121 <defcon1994@gmail.com>
2017-05-31 21:51:37 +0200

    Aby łapało wszystkie wyjątki

--- src/main/java/tripApp/Main.java
+++ src/main/java/tripApp/Main.java
@@ -1,27 +1,30 @@
 package tripApp;
 
-import com.microsoft.azure.storage.StorageException;
+import org.apache.log4j.Logger;
 import tripApp.config.StaticConfig;
 import tripApp.queue.QueueRunner;
 import tripApp.worker.IWorker;
 import tripApp.worker.presentation.VideoFromImagesWorker;
 import tripApp.worker.thumbnail.ResizeWorker;
 
-import java.net.URISyntaxException;
-import java.security.InvalidKeyException;
-
 /**
  * Created by piotr on 17.05.17.
  */
 public class Main {
-    public static void main(String[] args) throws InvalidKeyException, StorageException, URISyntaxException {
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
 
-        IWorker worker = new ResizeWorker(StaticConfig.getThumbnailBlobAzureConfig());
-        QueueRunner queueRunner = new QueueRunner(StaticConfig.getThumbnailGenQueueAzureConfig() , worker);
-        new Thread(queueRunner).start();
+    public static void main(String[] args) {
+        try {
+            IWorker worker = new ResizeWorker(StaticConfig.getThumbnailBlobAzureConfig());
+            QueueRunner queueRunner = new QueueRunner(StaticConfig.getThumbnailGenQueueAzureConfig(), worker);
+            new Thread(queueRunner).start();
 
-        IWorker videoFromImagesWorker = new VideoFromImagesWorker(StaticConfig.getThumbnailBlobAzureConfig());
-        QueueRunner presentationQueueRunner = new QueueRunner(StaticConfig.getPresentationGenQueueAzureConfig(), videoFromImagesWorker);
-        new Thread(presentationQueueRunner).start();
+            IWorker videoFromImagesWorker = new VideoFromImagesWorker(StaticConfig.getThumbnailBlobAzureConfig());
+            QueueRunner presentationQueueRunner =
+                    new QueueRunner(StaticConfig.getPresentationGenQueueAzureConfig(), videoFromImagesWorker);
+            new Thread(presentationQueueRunner).start();
+        } catch (Throwable t) {
+            LOGGER.error(t.getMessage(), t);
+        }
     }
 }
